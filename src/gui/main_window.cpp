ce782b26c0d64963cc49f56d1790af6beb4581b1
Yassen Efremov <yassen.efremov@gmail.com>
2024-05-17 22:13:31 +0300

    Add GUI for Keplerian elements

--- src/gui/main_window.cpp
+++ src/gui/main_window.cpp
@@ -45,6 +45,13 @@ void MainWindow::simulate() {
 		ui->VelSpinBoxZ->value()
 	};
 
+	double ecc = ui->EccSpinBox->value();
+	double sem_maj_ax = ui->SemMajAxSpinBox->value();
+	double inc = ui->IncSpinBox->value();
+	double ri_asc_node = ui->RiAscNodeSpinBox->value();
+	double arg_of_per = ui->ArgOfPerSpinBox->value();
+	double true_anom = ui->TrueAnomSpinBox->value();
+
 	double t_i = ui->StartTimeSpinBox->value();
 	double t_f = ui->FinishTimeSpinBox->value();
 	double t_step = t_f/10;
@@ -60,9 +67,11 @@ void MainWindow::simulate() {
 
 	// just for testing
 	orbsim::Satellite sat1(x0, v0);
-	orbsim::Satellite sat2(2.82977289672605e-05, 7000.19808952185,
-						   1.396263328559, 4.2146848510894e-08,
-						   6.28181250052071, 0.00137257340076574);
+	orbsim::Satellite sat2(
+		ecc, sem_maj_ax,
+		inc, ri_asc_node,
+		arg_of_per, true_anom
+	);
 
 	QString output;
 	for (int i = 0; i < integ->get_steps() - 1; i++) {
@@ -81,6 +90,13 @@ void MainWindow::load_example_values() {
 	orbsim::Vec3 x0{7000, 0.000001, -0.001608};
 	orbsim::Vec3 v0{0.000002, 1.310359, 7.431412};
 
+	double ecc = 2.82977289672605e-05;
+	double sem_maj_ax = 7000.19808952185;
+	double inc = 1.396263328559;
+	double ri_asc_node = 4.2146848510894e-08;
+	double arg_of_per = 6.28181250052071;
+	double true_anom = 	0.00137257340076574;
+
 	double t_i = 0;
 	double t_f = 86400;
 	double t_step = t_f/10;
@@ -96,4 +112,11 @@ void MainWindow::load_example_values() {
 	ui->StartTimeSpinBox->setValue(t_i);
 	ui->FinishTimeSpinBox->setValue(t_f);
 	ui->TimeStepSpinBox->setValue(t_step);
+
+	ui->EccSpinBox->setValue(ecc);
+	ui->SemMajAxSpinBox->setValue(sem_maj_ax);
+	ui->IncSpinBox->setValue(inc);
+	ui->RiAscNodeSpinBox->setValue(ri_asc_node);
+	ui->ArgOfPerSpinBox->setValue(arg_of_per);
+	ui->TrueAnomSpinBox->setValue(true_anom);
 }
