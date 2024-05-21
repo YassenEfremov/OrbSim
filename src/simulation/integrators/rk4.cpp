3712f42957c7a3143fc04c5451b68359995f6cf6
Yassen Efremov <yassen.efremov@gmail.com>
2024-05-21 11:24:18 +0300

    Use dimensionless units for integration

--- src/simulation/integrators/rk4.cpp
+++ src/simulation/integrators/rk4.cpp
@@ -16,6 +16,11 @@ RK4::RK4(double M, double R0, Vec3 x0, Vec3 v0,
 RK4 *RK4::copy() const { return new RK4(*this); }
 
 void RK4::integrate() {
+	// Norm the initial conditions
+	// this->time_arr = linspace?
+	this->delta_t /= this->T_dim;
+	this->pos_arr[0] /= this->R_dim;
+	this->vel_arr[0] /= this->V_dim;
 
 	struct {
 		Vec3 pos;
@@ -27,38 +32,40 @@ void RK4::integrate() {
 
 	for (int i = 0; i < steps - 1; i++) {
 		rk_slopes[0].pos = vel_arr[i];
-		rk_slopes[0].vel = - (M * G * this->pos_arr[i]) / (std::pow(this->pos_arr[i].len(), 3));
+		rk_slopes[0].vel = - this->pos_arr[i] / (std::pow(this->pos_arr[i].len(), 3));
 
 		pos_temp = this->pos_arr[i] + rk_slopes[0].pos * (delta_t/2);
 		vel_temp = this->vel_arr[i] + rk_slopes[0].vel * (delta_t/2);
 
 		rk_slopes[1].pos = vel_temp;
-		rk_slopes[1].vel = - (M * G * pos_temp) / (std::pow(pos_temp.len(), 3));
+		rk_slopes[1].vel = - pos_temp / (std::pow(pos_temp.len(), 3));
 
 		pos_temp = this->pos_arr[i] + rk_slopes[1].pos * (delta_t/2);
 		vel_temp = this->vel_arr[i] + rk_slopes[1].vel * (delta_t/2);
 
 		rk_slopes[2].pos = vel_temp;
-		rk_slopes[2].vel = - (M * G * pos_temp) / (std::pow(pos_temp.len(), 3));
+		rk_slopes[2].vel = - pos_temp / (std::pow(pos_temp.len(), 3));
 
 		pos_temp = this->pos_arr[i] + rk_slopes[2].pos * delta_t;
 		vel_temp = this->vel_arr[i] + rk_slopes[2].vel * delta_t;
 
 		rk_slopes[3].pos = vel_temp;
-		rk_slopes[3].vel = - (M * G * pos_temp) / (std::pow(pos_temp.len(), 3));
+		rk_slopes[3].vel = - pos_temp / (std::pow(pos_temp.len(), 3));
 
 		// Final next step estimation
 		this->pos_arr[i + 1] = pos_arr[i] + (rk_slopes[0].pos + 2*rk_slopes[1].pos + 2*rk_slopes[2].pos + rk_slopes[3].pos)/6 * delta_t;
 		this->vel_arr[i + 1] = vel_arr[i] + (rk_slopes[0].vel + 2*rk_slopes[1].vel + 2*rk_slopes[2].vel + rk_slopes[3].vel)/6 * delta_t;
 
 		// Convert back to kilometers
-		this->pos_arr[i] /= 1000;
-		this->vel_arr[i] /= 1000;
+		this->pos_arr[i] *= R_dim;
+		this->vel_arr[i] *= V_dim;
 	}
 
 	// Don't forget the last one
-	this->pos_arr[this->steps - 1] /= 1000;
-	this->vel_arr[this->steps - 1] /= 1000;
+	this->pos_arr[this->steps - 1] *= R_dim;
+	this->vel_arr[this->steps - 1] *= V_dim;
+
+	this->delta_t *= this->T_dim;
 }
 
 } // namespace orbsim
--- src/simulation/integrators/verlet.cpp
+++ src/simulation/integrators/verlet.cpp
@@ -16,19 +16,27 @@ Verlet::Verlet(double M, double R0, Vec3 x0, Vec3 v0,
 Verlet *Verlet::copy() const { return new Verlet(*this); }
 
 void Verlet::integrate() {
+	// Norm the initial conditions
+	// this->time_arr = linspace?
+	this->delta_t /= this->T_dim;
+	this->pos_arr[0] /= this->R_dim;
+	this->vel_arr[0] /= this->V_dim;
+
 	for (int i = 0; i < steps - 1; i++) {
-		Vec3 vel_half = this->vel_arr[i] + (- (M * G * this->pos_arr[i]) / (std::pow(this->pos_arr[i].len(), 3))) * (delta_t/2);
+		Vec3 vel_half = this->vel_arr[i] + (- this->pos_arr[i] / (std::pow(this->pos_arr[i].len(), 3))) * (delta_t/2);
 		this->pos_arr[i + 1] = this->pos_arr[i] + (vel_half) * delta_t;
-		this->vel_arr[i + 1] = vel_half + (- (M * G * this->pos_arr[i + 1]) / (std::pow(this->pos_arr[i + 1].len(), 3))) * (delta_t/2);
+		this->vel_arr[i + 1] = vel_half + (- this->pos_arr[i + 1] / (std::pow(this->pos_arr[i + 1].len(), 3))) * (delta_t/2);
 
 		// Convert back to kilometers
-		this->pos_arr[i] /= 1000;
-		this->vel_arr[i] /= 1000;
+		this->pos_arr[i] *= this->R_dim;
+		this->vel_arr[i] *= this->V_dim;
 	}
 
 	// Don't forget the last one
-	this->pos_arr[this->steps - 1] /= 1000;
-	this->vel_arr[this->steps - 1] /= 1000;
+	this->pos_arr[this->steps - 1] *= this->R_dim;
+	this->vel_arr[this->steps - 1] *= this->V_dim;
+
+	this->delta_t *= this->T_dim;
 }
 
 } // namespace orbsim
